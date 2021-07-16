254f95d1edfdbc5368b5f84bffbf9f9027a85e1b
Doğukan <78941156+qreardedwastaken@users.noreply.github.com>
2021-07-16 22:25:00 +0300

    Update main.php

--- src/qrearded/GamemodeUI/main.php
+++ src/qrearded/GamemodeUI/main.php
@@ -13,25 +13,28 @@
 *@ github.com/qreardedisback/GamemodeUI
 *@ GamemodeUI by qreardedisback & HardSiamang655
 */
-namespace qrearded\GamemodeUI;                                         
+namespace qrearded\GamemodeUI;
 
 use pocketmine\{Player, Server};
 use pocketmine\command\{Command, CommandSender, ConsoleCommandSender};
 use jojoe77777\FormAPI\{SimpleForm, CustomForm, ModalForm};
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as qc;
 
 class main extends PluginBase {
+  public function onEnable(){
 
+  }
   public function onCommand(CommandSender $q, Command $cmd, string $label, array $args): bool {
     if ($cmd->getName() === "gamemodeui" or "gmui") { 
     if ($q instanceof Player ){
-      if ($q->hasPermission("gamemodeui.hq") or $q->isOp()) {
+      if ($q->hasPermission("gmui.cmd") or $q->isOp()) {
       $this->gm($q);
       }else{
         $q->sendMessage("§8» §cYou do not have sufficient privileges to use this command.");
         }
       }else{
-        $q->sendMessage("» Use this command from within the game.");
+        $q->sendMessage("§8» §fUse this command in game.");
     }
    }
     return true;
@@ -45,21 +48,21 @@ class main extends PluginBase {
         case 0:
           $gm = $q->getGamemode();
           if ($gm === 0) {
-            $q->sendPopup("§8» §fYour game mode is already §aSurvival§f.");
+            $q->sendPopup("§8» §fYour game mode is already §6Survival§f.");
             return false;
           }
           
-             $q->sendPopup("§8» §fYour game mode has been changed to §aSurvival§f.");
+             $q->sendPopup("§8» §fYour game mode has been changed to §6Survival§f.");
                $q->setGamemode(0);
          break;
        
        case 1: 
          $gm = $q->getGamemode();
             if ($gm === 2) {
-            $q->sendPopup("§8» §fYour game mode is already §aAdventure§f.");
+            $q->sendPopup("§8» §fYour game mode is already §6Adventure§f.");
             return false;
           }
-         $q->sendPopup("§8» §fYour game mode has been changed to §aAdventure§f.");
+         $q->sendPopup("§8» §fYour game mode has been changed to §6Adventure§f.");
          $q->setGamemode(2);
          
          break;
@@ -67,10 +70,10 @@ class main extends PluginBase {
        case 2: 
          $gm = $q->getGamemode();
           if ($gm === 1) {
-            $q->sendPopup("§8» §fYour game mode is already §aCreative§f.");
+            $q->sendPopup("§8» §fYour game mode is already §6Creative§f.");
             return false;
           }
-         $q->sendPopup("§8» §fYour game mode has been changed to §aCreative§f.");
+         $q->sendPopup("§8» §fYour game mode has been changed to §6Creative§f.");
          $q->setGamemode(1);
          
          break;
@@ -78,34 +81,24 @@ class main extends PluginBase {
         case 3:
           $gm = $q->getGamemode();
             if ($gm === 3) {
-            $q->sendPopup("§8» §fYour game mode is already §aSpectator§f.");
+            $q->sendPopup("§8» §fYour game mode is already §6Spectator§f.");
             return false;
           }
-          $q->sendPopup("§8» §fYour game mode has been changed to §aSpectator§f.");
+          $q->sendPopup("§8» §fYour game mode has been changed to §6Spectator§f.");
           $q->setGamemode(3);
          
          break;
        }
      });
-     $gm = $q->getGamemode();
-     if ($gm === 0) {
-       $gm = "Survival";
-     }
-     if ($gm === 2) {
-       $gm = "Adventure";
-     }
-     if ($gm === 3) {
-       $gm = "Spectator";
-     }
-     if ($gm === 1) {
-       $gm = "Creative";
-     }
+   $gm = $q->getGamemode();
+     $arr = ['Survival', 'Creative', 'Adventure',  'Spectator'];
+     $gm = $arr[$gm];
      $form->setTitle("GamemodeUI");
-     $form->setContent("Choose your game mode.\nYour current game mode: §a$gm");
-     $form->addButton("Survival",0,"textures/blocks/dirt");
-     $form->addButton("Adventure",0,"textures/items/bow_pulling_2");
-     $form->addButton("Creative",0,"textures/blocks/diamond_block");
-     $form->addButton("Spectator",0,"textures/items/feather");
+     $form->setContent("Choose your game mode.\nYour current game mode: §6$gm");
+     $form->addButton("Survival",0,"textures/blocks/cobblestone");
+     $form->addButton("Adventure",0,"textures/blocks/brick");
+     $form->addButton("Creative",0,"textures/blocks/bedrock");
+     $form->addButton("Spectator",0,"textures/blocks/noteblock");
      $form->sendToPlayer($q);
    
    }
